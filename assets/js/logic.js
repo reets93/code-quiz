4d57f8e6391d46cd2596d87fe6e05197305a872a
reets <117296082+reets93@users.noreply.github.com>
2023-01-17 23:20:11 +0000

    adds final score on end-screen page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,6 +6,7 @@ var startScreen = document.querySelector("#start-screen")
 // var QTitle = document.querySelector("#question-title")
 var answerChoice = document.querySelector("#choices")
 var endScreen = document.querySelector("#end-screen")
+var finalScore = document.querySelector("#final-score")
 
 
 var secondsLeft = 71;
@@ -24,9 +25,9 @@ function startQuiz(event) {
 function iterateQs() {
     // does this need to be a whole while-loop, for-loop or an if loop (ifqNumber =10 then go to end screen, otherwise do the following)limited to up to question 10 and at last question goes to end screen?
     //start with index 0; 
-    for (x=0; x<quizQuestions.length; x++){ // remove loop just to have one displayed. 
-// how to get it so it only shows one q&a at a time rather than all together at once?
-        var qNumber = 0+x; // 0+x for loops
+    for (x = 0; x < quizQuestions.length; x++) { // remove loop just to have one displayed. 
+        // how to get it so it only shows one q&a at a time rather than all together at once?
+        var qNumber = 0 + x; // 0+x for loops
         var currentQ = "";
         currentQ = quizQuestions[qNumber].question;
         const QTitle = document.createElement("h2");
@@ -34,42 +35,41 @@ function iterateQs() {
         QTitle.appendChild(node);
         const QElement = document.getElementById("question-title")
         QElement.appendChild(QTitle);
-    
-    // this for-loop is to create the answers for quizQuestion[qNumber] on separate buttons
-    for (let i = 0; i < quizQuestions[qNumber].answers.length; i++) {
-        // append answer to button
-        const ansButton = document.createElement("button");
-        const node2 = document.createTextNode(quizQuestions[qNumber].answers[i])
-        const ansElement = document.getElementById("choices")
-        ansButton.appendChild(node2);
-        ansElement.appendChild(ansButton);
-        ansElement.className = "answerOption"
-        // ansElement.style = "display:inline-block;; margin: 5px; cursor: pointer; font-size: 100%; border-radius: 5px; padding: 2px 10px; color: white; border: 0; transition: background-color 0.1s;"
-    }
 
-    var answerOption = document.querySelector(".answerOption")
-    answerOption.addEventListener("click", function validate(event) {
-        console.log(event.target.textContent)
-        if (event.target.textContent == quizQuestions[qNumber].correctAnswer) {
-            // qNumber = qNumber + 1; // this is stored locally so not updating var qNumber = 0 when loops over
-            console.log("correct");
-            console.log(qNumber);
-            correct()
-        } else {
-            // qNumber = qNumber + 1;
-            console.log("incorrect");
-            secondsLeft - 10;
-            console.log(qNumber);
-            console.log(secondsLeft);
-            incorrect();
+        // this for-loop is to create the answers for quizQuestion[qNumber] on separate buttons
+        for (let i = 0; i < quizQuestions[qNumber].answers.length; i++) {
+            // append answer to button
+            const ansButton = document.createElement("button");
+            const node2 = document.createTextNode(quizQuestions[qNumber].answers[i])
+            const ansElement = document.getElementById("choices")
+            ansButton.appendChild(node2);
+            ansElement.appendChild(ansButton);
+            ansElement.className = "answerOption"
+            // ansElement.style = "display:inline-block;; margin: 5px; cursor: pointer; font-size: 100%; border-radius: 5px; padding: 2px 10px; color: white; border: 0; transition: background-color 0.1s;"
         }
-    })
-}
+
+        var answerOption = document.querySelector(".answerOption")
+        answerOption.addEventListener("click", function validate(event) {
+            console.log(event.target.textContent)
+            if (event.target.textContent == quizQuestions[qNumber].correctAnswer) {
+                // qNumber = qNumber + 1; // this is stored locally so not updating var qNumber = 0 when loops over
+                console.log("correct");
+                console.log(qNumber);
+                correct()
+            } else {
+                // qNumber = qNumber + 1;
+                console.log("incorrect");
+                secondsLeft - 10;
+                console.log(qNumber);
+                console.log(secondsLeft);
+                incorrect();
+            }
+        })
+    }
 }
 
 // TO DO:
 // tidy up the loop / if statement for the whole questions loop -> make sure that they clear each time, but display the next one
-// add a form + local storage with initials 
 // event listener results button to get to the high scores page
 // display highest score from local storage
 
@@ -112,19 +112,18 @@ function startTimer() {
 function endQuiz(event) {
     displayQuestions.className = "hide";
     endScreen.className = "visible"
+    finalScore.textContent = secondsLeft
     // enter initials + submit
-    // display score - end-screen class = visible?
 
-};
-
-submit.addEventListener("submit", submitResults)
-function submitResults(event){
-    // event.preventDefault()
-    localStorage.setItem("initials");
-    location.href = "https://www.google.com"
 }
-    
 
+// end screen submit 
+submit.addEventListener("submit", submitResults)
+function submitResults(event) {
+    // event.preventDefault()
+    // localStorage.setItem("initials");
+    location.href = "https://www.google.com"
+};
 
 function resultsBoard() {
 
