0b5120bd7508cef185717b800178b27380ff4ca1
reets <117296082+reets93@users.noreply.github.com>
2023-01-17 17:47:32 +0000

    tried some methods for displaying all the Q/As

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -23,15 +23,17 @@ function startQuiz(event) {
 function iterateQs() {
     // does this need to be a whole while-loop, for-loop or an if loop (ifqNumber =10 then go to end screen, otherwise do the following)limited to up to question 10 and at last question goes to end screen?
     //start with index 0; 
-    var qNumber = 0;
-    var currentQ = "";
-    currentQ = quizQuestions[qNumber].question;
-    const QTitle = document.createElement("h2");
-    const node = document.createTextNode(currentQ) // displays the question 
-    QTitle.appendChild(node);
-    const QElement = document.getElementById("question-title")
-    QElement.appendChild(QTitle);
-
+    // for (x=0; x<quizQuestions.length; x++){ // remove loop just to have one displayed. 
+// how to get it so it only shows one q&a at a time rather than all together at once?
+        var qNumber = 0; // 0+x for loops
+        var currentQ = "";
+        currentQ = quizQuestions[qNumber].question;
+        const QTitle = document.createElement("h2");
+        const node = document.createTextNode(currentQ) // displays the question 
+        QTitle.appendChild(node);
+        const QElement = document.getElementById("question-title")
+        QElement.appendChild(QTitle);
+    
     // this for-loop is to create the answers for quizQuestion[qNumber] on separate buttons
     for (let i = 0; i < quizQuestions[qNumber].answers.length; i++) {
         // append answer to button
@@ -48,12 +50,12 @@ function iterateQs() {
     answerOption.addEventListener("click", function validate(event) {
         console.log(event.target.textContent)
         if (event.target.textContent == quizQuestions[qNumber].correctAnswer) {
-            qNumber = qNumber + 1;
+            // qNumber = qNumber + 1; // this is stored locally so not updating var qNumber = 0 when loops over
             console.log("correct");
             console.log(qNumber);
             correct()
         } else {
-            qNumber = qNumber + 1;
+            // qNumber = qNumber + 1;
             console.log("incorrect");
             secondsLeft - 10;
             console.log(qNumber);
@@ -62,6 +64,7 @@ function iterateQs() {
         }
     })
 }
+// }
 
 // TO DO:
 // tidy up the loop / if statement for the whole questions loop -> make sure that they clear each time, but display the next one
@@ -69,17 +72,19 @@ function iterateQs() {
 // event listener results button to get to the high scores page
 // display highest score from local storage
 
+
+//add a 'correct' message at the bottom of the answers
 function correct() {
     const correctMessage = document.createElement("h3");
     correctMessage.textContent = "Well done! That's correct"
     displayQuestions.appendChild(correctMessage);
     correctMessage.style = "color: green"
     console.log(correctMessage)
-    //add a 'correct' message at the bottom of the answers
 }
 
+//add an 'incorrect' message at the bottom of the answers
 function incorrect() {
-    secondsLeft = secondsLeft-10
+    secondsLeft = secondsLeft - 10
     const incorrectMessage = document.createElement("h3");
     incorrectMessage.textContent = "That's incorrect"
     displayQuestions.appendChild(incorrectMessage);
@@ -87,6 +92,7 @@ function incorrect() {
     console.log(incorrectMessage)
 }
 
+// timer function 
 function startTimer() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
@@ -112,13 +118,3 @@ function endQuiz(event) {
 function resultsBoard() {
     //event listener? go to highscores
 };
-
-
-// list of questions
-// go through them in a loop 
-// chosen answer --> create a variable where the index selected goes into it. then do an if loop to see if chosen answer and correct answer are equal. 
-// correctAnswer --> proceed to next question
-// incorrectAnswer --> deduct time 
-// if (chosen answer !== correctAnswer) {
-    // secondsLeft -10;
-// }
